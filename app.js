497323395f52d455f1957e6fa190c58044a25a6c
iargent <iainargent@googlemail.com>
2016-08-03 10:16:26 +0100

    completed

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 /**
  * Created by iain on 03/08/2016.
  */
+
+require.config({
+    paths: {
+        "jquery": "https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min",
+        "underscore": "lib/underscore",
+    }
+});
+
+require(['lib/modules/template'], function (template) {
+    template.showName("Jack");
+});
