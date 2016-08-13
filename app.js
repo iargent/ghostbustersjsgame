21cbdce0e90be9484267a862308254cd47a54f64
Iain Argent <iainargent@googlemail.com>
2016-08-13 21:48:09 +0100

    fixing warnings

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ require(['lib/modules/template'], function () {
 
     var setGhost = function() {
         return Math.floor(Math.random()*10);
-    }
+    };
 
     $(".button").on('click', play);
 
@@ -23,39 +23,40 @@ require(['lib/modules/template'], function () {
         var score=0;
         var delay=200;
         var dead=false;
-        $("#controller").text(0);
+        var con=$("#controller");
+        con.text(0);
 
         document.onkeydown = function (e) {
             switch (e.keyCode) {
                 case 77:
-                    var v = $("#controller").text();
-                    $("#controller").text(v==9?0:++v);
+                    var v = con.text();
+                    con.text(v==9?0:++v);
                     break;
                 case 88:
-                    var v = $("#controller").text();
-                    if (ghost==v) {
+                    if (ghost==con.text()) {
                         $("#"+(ghostpos-1)).text(" ");
                         score+=(ghost+200-delay);
                         delay--;
                         ghost=setGhost();
                         ghostpos=0;
                     }
+                    break;
                 default:
                     console.log(e);
             }
-        }
+        };
 
         function iter() {
             if (ghostpos > 0) $("#" + (ghostpos - 1)).text(" ");
             $("#" + ghostpos).text(ghost);
             if (ghostpos == 20) {
                 dead = true;
-                if (dead) alert("You scored "+score+" points.");
+                if (dead) alert("You've been slimed!\nYou scored "+score+" points.");
                 return;
             }
             ghostpos++;
             setTimeout(iter, delay);
-        };
+        }
         iter();
     }
 });
